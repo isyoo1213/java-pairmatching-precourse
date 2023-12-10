cbc6803b3119b39a2924ebe494dcc6f3d06ad395
isyoo1213 <moethns@gmail.com>
2023-12-10 21:53:46 +0900

    feat: Crews 모델 클래스 추가 및 코스/크루 반환 구현 model/Crews - provideCrewsInfo() docs/README.md - 기능 명세 변경 및 구현 체크

---
+++ src/main/java/pairmatching/model/Crews.java
@@ -0,0 +1,33 @@
+package pairmatching.model;
+
+import pairmatching.constants.Courses;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Crews {
+
+    Map<Courses, List<Crew>> crewsInfo;
+
+    public Crews(Courses course, List<Crew> crews) {
+        Map<Courses, List<Crew>> tempMap = new LinkedHashMap<>();
+        tempMap.put(course, crews);
+        crewsInfo = Map.copyOf(tempMap);
+    }
+
+    public Map<String, List<String>> provideCrewsInfo() {
+        Map<String, List<String>> tempMap = new LinkedHashMap<>();
+        for (Map.Entry<Courses, List<Crew>> crewInfo : crewsInfo.entrySet()) {
+            String courseName = crewInfo.getKey().getCourseName();
+            List<String> crewNames = new ArrayList<>();
+            for (Crew crew : crewInfo.getValue()) {
+                String crewName = crew.provideCrewName();
+                crewNames.add(crewName);
+            }
+            tempMap.put(courseName, crewNames);
+        }
+        return Map.copyOf(tempMap);
+    }
+}
