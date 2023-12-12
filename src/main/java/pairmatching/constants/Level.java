ec12887d5dbbfadebce3563950fd576b3191b92a
isyoo1213 <moethns@gmail.com>
2023-12-12 17:47:57 +0900

    feat: Level 변수명 오류수정/문자 레벨정보 반환 구현 constants/Level - getLevelWithNumber() 추가 - 전역변수 추가   - LEVEL_STRING   - LEVEL_WITH_NUMBERS   - LEVEL_BY_NUMBER

--- src/main/java/pairmatching/constants/Level.java
+++ src/main/java/pairmatching/constants/Level.java
@@ -1,14 +1,29 @@
 package pairmatching.constants;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public enum Level {
 
-    LEVE_1(1, List.of("자동차 경주", "로또", "숫자야구게임")),
-    LEVE_2(2, List.of("장바구니", "결제", "지하철노선도")),
-    LEVE_3(3, List.of()),
-    LEVE_4(4, List.of("성능개선", "배포")),
-    LEVE_5(5, List.of());
+    LEVEL_1(1, List.of("자동차 경주", "로또", "숫자야구게임")),
+    LEVEL_2(2, List.of("장바구니", "결제", "지하철노선도")),
+    LEVEL_3(3, List.of()),
+    LEVEL_4(4, List.of("성능개선", "배포")),
+    LEVEL_5(5, List.of());
+
+    public static final String LEVEL_STRING = "레벨";
+    public static final List<String> LEVEL_WITH_NUMBERS =
+            Arrays.stream(Level.values())
+                    .map((level) -> LEVEL_STRING + level.getLevelNumber())
+                    .collect(Collectors.toList());
+    public static final Map<String, Level> LEVEL_BY_NUMBER =
+            Collections.unmodifiableMap(Stream.of(values())
+                    .collect(Collectors.toMap(Level::getLevelWithNumber, Function.identity())));
 
     private final int levelNumber;
     private final List<String> missions;
@@ -20,6 +35,10 @@ public enum Level {
 
     public int getLevelNumber() {
         return levelNumber;
+    }
+
+    public String getLevelWithNumber() {
+        return LEVEL_STRING + levelNumber;
     }
 
     public List<String> getMissions() {
