b46e859485f2b1acc13ae22de56955bfdf9055dc
isyoo1213 <moethns@gmail.com>
2023-12-12 15:40:29 +0900

    feat: ExceptionMessages 오류케이스 추가 constants/ExceptionMessages - WRONG_COURSE_INPUT - WRONG_LEVEL_INPUT - WRONG_MISSION_INPUT

--- src/main/java/pairmatching/constants/ExceptionMessages.java
+++ src/main/java/pairmatching/constants/ExceptionMessages.java
@@ -4,7 +4,11 @@ public enum ExceptionMessages {
     NULL_INPUT("null은 입력할 수 없습니다."),
     EMPTY_INPUT("값을 입력해주세요."),
     WRONG_FEATURE_INPUT("1~3, Q를 입력해주세요."),
-    WRONG_FILE_FORMAT("파일을 읽을 수 없습니다.");
+    WRONG_FILE_FORMAT("파일을 읽을 수 없습니다."),
+    WRONG_COURSE_INPUT("존재하지 않는 코스입니다."),
+    WRONG_LEVEL_INPUT("존재하지 않는 레벨입니다."),
+    WRONG_MISSION_INPUT("레벨에 포함되지 않는 미션입니다."),
+    WRONG_MISSION_FORMAT("쉼표 구분자(,)를 이용해 코스, 레벨, 미션을 입력해주세요.");
 
     private final String exceptionMessage;
     private final String ERROR_TAG = "[ERROR] ";
